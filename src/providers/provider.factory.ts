13643fadc35a99844346f053cd779fb58156da3a
Raouf Rahiche <mrrahicheraouf@gmail.com>
2025-01-16 17:47:28 +0000

    fix: correct import path for AptosProvider in provider factory

--- src/providers/provider.factory.ts
+++ src/providers/provider.factory.ts
@@ -1,7 +1,7 @@
 
 import { BlockchainProvider } from '../core/types';
+import { AptosProvider } from './aptos/aptos.provider';
 import { XRPLProvider } from './xrpl/xrpl.provider';
-import { AptosProvider } from './aptos.provider';
 
 export class ProviderFactory {
   static createProvider(blockchain: string, network: string, wallet: string): BlockchainProvider {
