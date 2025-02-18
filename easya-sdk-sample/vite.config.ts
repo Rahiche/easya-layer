2074bca2b69175aed688e13f237a49c4e2c5e1f6
Raouf Rahiche <mrrahicheraouf@gmail.com>
2025-02-18 17:09:03 +0800

    chore: update @easya/layer-ui-react version and improve build scripts in easya-react package

--- easya-sdk-sample/vite.config.ts
+++ easya-sdk-sample/vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@easya/layer-ui-react': path.resolve(__dirname, '../packages/easya-react')
+    }
+  },
 });
